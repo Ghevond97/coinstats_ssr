3b86821bbc0c64920fd4a323001259e59d033375
Ghevond97 <ghevond_mirzakhanyan@edu.aua.am>
2018-11-01 18:30:05 +0400

    container added, and Table of Stats passed to App js, which is then imported to index.js of client folder. the hydrate() method then is responsible for ssr and connection between server and client

---
+++ src/client/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TableOfStats from './containers/TableOfStats';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TableOfStats />
+      </div>
+    );
+  }
+}
+
+export default App;
